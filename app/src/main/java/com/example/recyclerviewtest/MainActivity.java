7e308352cb9244aeb859569c6a795f04cdb774b7
MrHuang-coder <s879407781@qq.com>
2020-11-25 11:12:57 +0800

    第一次提交

---
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFruits(); // 初始化水果数据
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3,
+                StaggeredGridLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 5; i++) {
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
+            fruitList.add(banana);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+
+        }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(name);
+        }
+        return builder.toString();
+    }
+}
