9918ca8690e95f1146c0a6758a44295ceae730d1
MrHuang-coder <s879407781@qq.com>
2020-11-25 11:44:41 +0800

    修改MainActivity

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initFruits(); // 初始化水果数据
